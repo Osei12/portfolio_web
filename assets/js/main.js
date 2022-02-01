9c1ce3c88ef36b9cf5ec1796d9e843dfca1997f1
osei21 <oseiadu6@gmai.com>
2022-02-01 23:16:45 +0000

    page changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,55 @@
+// NAVBAR 
 
-//RESPONSIVE NAVIGATION MENU
-const menuBtn = document.querySelector(".menu-btn");
-const navigation = document.querySelector(".navigation");
+const menu = document.querySelector('.menu')
+const close = document.querySelector('.close')
+const nav = document.querySelector('nav')
 
-menuBtn.addEventListener("click", () => {
-    menuBtn.classList.toggle("active");
-    navigation.classList.toggle("active");
-});
+menu.addEventListener('click', () => {
+    nav.classList.add('open-nav')
+})
+close.addEventListener('click', () => {
+    nav.classList.remove('open-nav')
+})
 
-// var nav = document.querySelector('header');
-// window.addEventListener('scroll', function () {
-//     if (window.pageYOffset > 100) {
-//         nav.classList.add('sticky', 'shadow');
-//     } else {
-//         nav.classList.remove('sticky', 'shadow');
-//     }
-// });
+
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        center: true,
+        nav: true,
+        navText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1,
+            },
+            1000: {
+                items: 4,
+
+            }
+        }
+    })
+})
+
+
+const toTop = document.querySelector(".to-top-btn");
+const Top = document.getElementById("to-top")
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active")
+    }
+})
+
+Top.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+})
