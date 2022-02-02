15d41cd6243924ffe108b0b37e655a74c21a9feb
osei21 <oseiadu6@gmai.com>
2022-02-02 22:02:22 +0000

    scroll to top added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// NAVBAR 
+// NAVBAR TOGGLE
 
 const menu = document.querySelector('.menu')
 const close = document.querySelector('.close')
@@ -11,36 +11,10 @@ close.addEventListener('click', () => {
     nav.classList.remove('open-nav')
 })
 
-
-$(document).ready(function () {
-    $('.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 10,
-        responsiveClass: true,
-        center: true,
-        nav: true,
-        navText: [
-            "<i class='fa fa-angle-left'></i>",
-            "<i class='fa fa-angle-right'></i>"
-        ],
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 1,
-            },
-            1000: {
-                items: 4,
-
-            }
-        }
-    })
-})
-
+// SCROLL TO TOP
 
 const toTop = document.querySelector(".to-top-btn");
-const Top = document.getElementById("to-top")
+const Top = document.getElementById("to-top");
 
 window.addEventListener("scroll", () => {
     if (window.pageYOffset > 100) {
@@ -53,3 +27,7 @@ window.addEventListener("scroll", () => {
 Top.addEventListener("click", () => {
     document.documentElement.scrollTop = 0;
 })
+
+
+
+
