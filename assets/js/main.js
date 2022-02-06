6481ea60ffd80adb39c412701c6aa348745dbd8d
osei21 <oseiadu6@gmai.com>
2022-02-06 23:09:35 +0000

    Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
 // NAVBAR TOGGLE
 
 const menu = document.querySelector('.menu')
